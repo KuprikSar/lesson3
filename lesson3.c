c9185e1452b5e5e98e8d7095937c14e6680cad0e
KuprikSar <i@avp1991.ru>
2023-10-09 16:04:30 +0300

    firstStart

---
+++ lesson3.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+/*
+int main (void) {
+
+    int a = 1, b = 1, expr = 4;
+    switch (expr)
+    {
+        case 2:
+            a *= 2;
+            break;
+        
+        case 3:
+            a += 5;
+            break;
+
+        default:
+            a = -1;
+            break;
+    }
+
+    printf("a = %d\n", a);
+    return 0;
+}
+*/
+
+/*
+int main (void) {
+
+    int input;
+    scanf("%d", &input);
+    switch (input)
+    {
+    case 1: printf("one\n"); break;
+    case 2: printf("two\n"); break;
+    case 3: printf("three\n"); break;
+    case 4: printf("four\n"); break;
+    default:
+        printf("more four\n");
+    }
+    return 0;
+}
+*/
+
+/*
+int main (void) {
+
+    int n;
+    n = 1;
+    while (n <= 5)
+    {
+        printf("%d\n", n);
+        n++;
+    }
+    
+
+
+    return 0;
+}
+*/
+
+/*
+int main (void) {
+
+    int a = 4, b = 6, cnt = 0;
+    while (a < b)
+    {
+        a++;
+        cnt++;
+    }
+    printf("a = %d, b = %d, cnt = %d", a, b, cnt);
+
+    return 0;
+}
+*/
+
+int main (void) {
+
+    int a = 4, b = 6, cnt = 0;
+    while (a < b)
+    {
+        a++;
+        cnt++;
+    }
+    printf("a = %d, b = %d, cnt = %d", a, b, cnt);
+
+    return 0;
+}
